5c0028a78e665be40fd7545d3538b74fdb65ce16
veedee <43088920+veedee2000@users.noreply.github.com>
2020-03-25 03:49:32 +0530

    Quiz Structure Completed. Few more questions to add

--- App.js
+++ App.js
@@ -1,44 +1,55 @@
-const checks = document.querySelectorAll('.checkbox');
-const menu = document.querySelector('.menu');
-const topicButton = document.querySelector('.topic-button');
+const startButton = document.querySelector('.start-button');
+const guidlines = document.querySelector('.guidlines');
 
 const quiz = document.querySelector('.quiz');
 const timer = document.querySelector('.timer');
 const timerEnd = document.querySelector('.timer-end');
+const congratulation = document.querySelector('.congratulation')
 
-const correctAnswers = ['1','1','2'];
+let selectedQuestions = [];
+let correctAnswers = [];
 const user = document.querySelectorAll('input');
-const marks = document.querySelectorAll('.assign-mark');
 let remaining = 3;
+const questions = document.querySelectorAll('.questions');
 
-const tick = '\u2705';
-const wrong = '\u274C';
+const intro = document.querySelector('.intro');
+const outro = document.querySelector('.outro');
+const endResult = document.querySelector('.end-result');
 
-let givenTopicArray = ['Greedy','Dynamic Programming','Binary Search','Tree','DFS','BFS','Heap'];
-let topicArray = [];
-checks.forEach((name,index) =>{
-    const currentCheckBox = name.querySelector('input');
-    currentCheckBox.addEventListener('change',e =>{
-        if(currentCheckBox.checked == 0){
-            for(let i = 0;i < topicArray.length;i++){
-                if(topicArray[i] === givenTopicArray[index]) topicArray.splice(i,1);
-            }
-        }
-        else if(topicArray.length === 3){
-            alert('Max 3 topics at a time');
-            currentCheckBox.checked = 0;
-        }
-        else topicArray.push(givenTopicArray[index]);
-    });
-});
+// Initailizing timevariables ----------------------------------
 
-let timeNow = 60 * 60;
+let timeNow = 60 * 15;
 let hour,minute,second,addHour,addMinute,addSecond;
 
-topicButton.addEventListener('click',e => {
-    menu.classList.add('d-none');
+// Disabling tab change -----------------------------------------
+
+$(window).blur(function() {
+    if(timeNow > 0 && !(timer.classList.contains('d-none'))){
+        alert('You have been disqualifeid due to changing tabs');
+        location.reload();
+    }
+ });
+
+ // Main Page Button ----------------------------------------------
+
+startButton.addEventListener('click',e => {
+    guidlines.classList.add('d-none');
     quiz.classList.remove('d-none');
     timer.classList.remove('d-none');
+    let sz = 0;
+    while(sz < 3){
+        let val = Math.floor(Math.random() * 5);
+        if(selectedQuestions.length > 0 && selectedQuestions.includes(val)) continue;
+        else{
+            selectedQuestions.push(val);
+            correctAnswers.push(q[val].answer);
+            questions[sz].innerText = q[val].statement;
+            sz++;
+        }
+    }
+
+    // Timer Function  -------------------------------------------------
+
     const startTimer = setInterval(() => {
         hour = Math.floor(timeNow / 3600);
         minute = Math.floor((timeNow - (hour * 3600)) / 60);
@@ -51,38 +62,148 @@ topicButton.addEventListener('click',e => {
         else addSecond = `:${second}`;
 
         timer.querySelector('span').innerText = addHour + addMinute + addSecond;
-        if(timeNow === 599){
+        if(timeNow === 299){
             timer.querySelector('span').classList.remove('bg-dark');
             timer.querySelector('span').classList.add('bg-danger');
         }
 
         if(timeNow === 0){
-            clearInterval(startTimer);
             timer.classList.add('d-none');
-            timerEnd.classList.remove('d-none');
             user.forEach((name,index) =>{
                 if(name.type === 'text'){
                     name.disabled = 'disabled'; 
                 }
             });
+            intro.classList.add('d-none');
+            quiz.classList.add('d-none');
+            if(remaining > 0){
+                outro.classList.remove('d-none');
+                timerEnd.classList.remove('d-none');
+                outputMissed();
+            }
+            else congratulation.classList.remove('d-none');
+            clearInterval(startTimer);
         }
         timeNow--;
     },1000);
-});
+    });
+
+// Answer Checker Event Handler----------------------------------------
 
 user.forEach((name,index) =>{
     if(name.type === 'submit'){
         name.addEventListener('click',e =>{
             e.preventDefault();
-            if(user[index - 1].value === correctAnswers[(index - 8) / 2]){
-                marks[(index - 8) / 2].textContent = tick;
-                user[index - 1].disabled = "disabled";
-                // if(--remaining === 0)  
+            if(correctAnswers[(index - 1) / 2].includes(user[index - 1].value)){
+                remaining--;
+                eachCorrect(index)
+                if(remaining === 0){
+                    timeNow = 0;
+                }
             }
             else{
-                marks[(index - 8) / 2].textContent = wrong;
+                eachWrong(index);
             }
         });
     }
 });
 
+const eachCorrect = (i) =>{
+    user[i - 1].disabled = "disabled";
+    questions[(i - 1)/ 2].classList.add('bg-success');
+    questions[(i - 1)/ 2].classList.add('p-2');
+    questions[(i - 1)/ 2].classList.add('round-large');
+};
+
+const eachWrong = (i) =>{
+    questions[(i - 1)/ 2].classList.add('bg-danger');
+    questions[(i - 1)/ 2].classList.add('p-2');
+    questions[(i - 1)/ 2].classList.add('round-large');
+};
+
+const outputMissed = () =>{
+    user.forEach((name,index) =>{
+        if(name.type === 'text'){
+            if(name.value !== correctAnswers[index / 2]){
+                endResult.innerHTML += `<p style="font-size: large;" class="p-3">Q${(index / 2) + 1} : Correct Answer = ${correctAnswers[index / 2]}</p>`
+            }
+        }
+    });
+};
+
+
+
+// Questions ------------------------------------------------
+
+
+const q = [
+    {
+        statement: `
+            int i, j, k = 0; 
+            for (i = n / 2; i <= n; i++) { 
+                for (j = 2; j <= n; j = j * 2) { 
+                    k = k + n / 2; 
+                } 
+            }
+                    `,
+        answer: [`nlogn`]
+    },
+    {
+        statement:`
+            int a = 0, i = N; 
+            while (i > 0) { 
+                a += i; 
+                i /= 2; 
+            }
+                    `,
+        answer: [`logn`]
+    },
+    {
+        statement:`     
+                    { 3T(n-1), if n>0 },
+            T(n) =   
+                    { 1, otherwise }
+                        `,
+        answer: [`3^n`]
+    },
+    {
+        statement: `    
+                    { 2T(n-1) - 1, if n>0 },
+            T(n) =   
+                    { 1, otherwise }
+                    `,
+        answer: [`1`,1]
+    },
+    {
+        statement: `
+            class Base1 { 
+                public: 
+                    Base1() 
+                    { cout << " Base1's constructor called,";  } 
+            }; 
+                
+            class Base2 { 
+                public: 
+                    Base2() 
+                    { cout << "Base2's constructor called,";  } 
+            }; 
+                
+            class Derived: public Base1, public Base2 { 
+                public: 
+                    Derived() 
+                    {  cout << "Derived's constructor called,";  } 
+            }; 
+                
+            int main() 
+            { 
+                Derived d; 
+                return 0; 
+            }
+                    `,
+        answer: [`Base1’s constructor called,Base2’s constructor called,Derived’s constructor called,`]
+        
+    }
+    // {
+    //     statement: ``
+    // }
+];
